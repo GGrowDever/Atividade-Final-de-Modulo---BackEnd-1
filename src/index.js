9bb99602347645eb46d44e0b50d12bf85800edb1
GGrowDever <gabrielgrowdever@gmail.com>
2023-09-05 22:39:42 -0300

    instalado cors para teste

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import usersRouter from './routes/users';
 import notesRouter from './routes/notes';
@@ -6,6 +7,12 @@ import notesRouter from './routes/notes';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
+
+// const corsOptions = {
+//      origin: 'http://localhost:3333'
+// }
 
 app.use('/users', usersRouter);
 app.use('/notes', notesRouter);
